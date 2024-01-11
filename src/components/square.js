f9ca68996778c0cf1ecdae6a4f42da850334d551
AneeOri <81825413+AneeOri@users.noreply.github.com>
2024-01-11 13:35:06 -0600

    [.] Update Square
    Adding Props value, onSquareClick

--- src/components/square.js
+++ src/components/square.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Square(){
+export default function Square(value, onSquareClick){
     return(
      <button className="square" onClick={onSquareClick}>
         {value}
