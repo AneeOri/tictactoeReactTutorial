8a82da80649e29dc6ec9333366d24f1d120d054f
AneeOri <81825413+AneeOri@users.noreply.github.com>
2024-01-11 13:21:56 -0600

    [.] Update Board component
    adding a handle click function in Board Component

--- src/components/board.js
+++ src/components/board.js
@@ -1,9 +1,20 @@
 import React from "react";
+import CalculateWinner from "./calculateWinner";
 
-export default function Board(){
+export default function Board({xIsNext, squares, onPlay}){
 
     function handleClick(i){
-
+       if(CalculateWinner(squares) || squares[i]){
+        return;
+       }
+       const nextSquares = squares.slice();
+       if(xIsNext){
+        nextSquares[i] = 'X';
+       }
+       else{
+        nextSquares[i]='O';
+       }
+       onPlay(nextSquares);
     }
 
     return(
