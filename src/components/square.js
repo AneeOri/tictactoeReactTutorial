93ba5406a1f94cebcfc67cfc384178bf922c2ec1
AneeOri <81825413+AneeOri@users.noreply.github.com>
2024-01-26 20:44:11 -0600

    [.] Update Square Component

--- src/components/square.js
+++ src/components/square.js
@@ -1,15 +1,10 @@
 import React from "react";
 import { useState } from "react";
 
-export default function Square(){
+export default function Square({value, onSquareClick}){
 
-    const [value, setValue] = useState(null);
-
-    function handleClick(){
-        setValue('X'); //RE-RENDER THE SQUARE
-    }
-    return( 
-          <button className="square" onClick={handleClick}>
-            {value}
-          </button>);
+    return( <button className="square" onClick={onSquareClick}>
+              {value}
+            </button>
+          );
 }
